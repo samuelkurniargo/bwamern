9c6b598e7cf552da7eb96e39b589514b2260dd86
Samuel Argo <samuel.kurniago@ti.ukdw.ac.id>
2022-04-02 11:08:52 +0700

    Finish Details Page and fix minor bug in formatDate

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
-import "assets/scss/style.scss";
 import LandingPage from "pages/LandingPage";
+import DetailsPage from "pages/DetailsPage";
+
+import "assets/scss/style.scss";
 
 function App() {
   return (
     <div className="App">
       <Router>
-          <Route path="/" component={LandingPage} />
+          <Route exact path="/" component={LandingPage} />
+          <Route exact path="/properties/:id" component={DetailsPage} />
       </Router>
     </div>
   );
